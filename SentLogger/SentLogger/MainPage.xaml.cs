c25ddb362e5dc991551876b1c70917c22f05ce2c
Gustav Andersson <gastav3@hotmail.com>
2018-05-08 09:22:25 +0200

    Updated whole project

---
+++ SentLogger/SentLogger/MainPage.xaml.cs
@@ -0,0 +1,23 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Globalization;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using SentLogger.Resources;
+using Xamarin.Forms;
+
+namespace SentLogger
+{
+	public partial class MainPage : ContentPage
+	{
+		public MainPage()
+		{
+			InitializeComponent();
+		    AppResource.Culture = new CultureInfo("sv"); // Change Language
+		    InitializeComponent(); // Run this again after chaning language
+
+            // label.Text = AppResource.Title;
+        }
+	}
+}
